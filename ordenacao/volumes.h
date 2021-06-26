cbc4ad31a1a2fceb30a78c23b1f9e84757443ffa
rafaelst97 <42190573+rafaelst97@users.noreply.github.com>
2021-06-26 15:04:21 -0300

    Criação do Bubble sort

--- ordenacao/volumes.h
+++ ordenacao/volumes.h
@@ -16,12 +16,27 @@ void tamanho_volume() {
 	int maior = 100;
 	int menor = 1;
 
+	//Preenchimento do vetor
 	for (int i = 0; i < 100; i++) {
 		teste[i] = rand() % (maior - menor + 1) + menor;
 	}
 
 	for (int j = 0; j < 100; j++) {
 		cout << j+1 << ": " << teste[j] << endl;
+	}
+
+	for (int i = 0; i < 100; i++) {
+		for (int j = 0; j < 99; j++) {
+			if (teste[j] > teste[j + 1]) {
+				int aux = teste[j + 1];
+				teste[j + 1] = teste[j];
+				teste[j] = aux;
+			}
+		}
+	}
+
+	for (int j = 0; j < 100; j++) {
+		cout << j + 1 << ": " << teste[j] << endl;
 	}
 
 }
