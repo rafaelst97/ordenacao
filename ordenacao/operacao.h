9af6a7dc317a49ca3c78906cede805888ea89acb
rafaelst97 <42190573+rafaelst97@users.noreply.github.com>
2021-06-26 17:15:10 -0300

    Começo do código de ordenação

--- ordenacao/operacao.h
+++ ordenacao/operacao.h
@@ -7,14 +7,24 @@
 
 using namespace std;
 
+//Operacao responsavel pela criacao do vetor e ordenacao
 void ordenacao(int metodo_de_ordenacao, int selecao_tamanho) {
 
-	if (selecao_tamanho == 1)
+	if (selecao_tamanho == 1) {
 
-	srand((unsigned)time(0)); //para gerar números aleatórios reais.
+		const int MAX = 10;
+		int vetor[MAX];
+		int maior = MAX;
+		int menor = 1;
+		srand((unsigned)time(0)); //para gerar numeros aleatorios reais
 
-	//Preenchimento do vetor
-	for (int i = 0; i < 100; i++) {
-		teste[i] = rand() % (maior - menor + 1) + menor;
+		//Preenchimento do vetor
+		for (int i = 0; i < MAX; i++) {
+			vetor[i] = rand() % (maior - menor + 1) + menor;
+		}
+
+		if (metodo_de_ordenacao == 1) {
+
+		}
 	}
 }
